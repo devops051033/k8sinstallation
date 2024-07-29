f42120066b2b7cc2efc159d2760e5e5c4bcd8d53
mohammad <devops051033@gmail.com>
2024-07-29 01:32:34 +0000

    script update. line added EOF

--- containerRunTimePreRequisiteAndContainerd_installation.sh
+++ containerRunTimePreRequisiteAndContainerd_installation.sh
@@ -34,3 +34,4 @@ sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
+sudo systemctl status containerd
